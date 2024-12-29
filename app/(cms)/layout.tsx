14fc3a5cbd14c918cbf1bbe8be3c324c37e58276
Sheikh Omer <ska.omer@yahoo.com>
2024-12-29 18:16:54 +0000

    working copy

--- app/(cms)/layout.tsx
+++ app/(cms)/layout.tsx
@@ -6,8 +6,8 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
+
+        <>{children}</>
+
     );
 }
